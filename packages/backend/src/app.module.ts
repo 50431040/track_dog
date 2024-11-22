0d4d301aeb02da7ac645c438b6cd84798e6d096d
Stahsf <50431040@qq.com>
2024-11-22 21:41:07 +0800

    feat: 配置文件

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // config
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'production' ? '.env.prod' : '.env.local',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
