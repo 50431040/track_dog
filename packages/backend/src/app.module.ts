831129e6137bd5dfb435f7389bcbeda2f5f92d9b
Stahsf <50431040@qq.com>
2024-11-22 21:48:34 +0800

    feat: 消息队列

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,8 +1,9 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {ConfigModule, ConfigService} from '@nestjs/config';
-import {RedisModule} from "@liaoliaots/nestjs-redis";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
@@ -23,9 +24,19 @@ import {RedisModule} from "@liaoliaots/nestjs-redis";
         },
       }),
     }),
+    // 队列
+    BullModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        redis: {
+          host: configService.get('REDIS_HOST'),
+          port: parseInt(configService.get('REDIS_PORT'), 10),
+          password: configService.get('REDIS_PASSWORD'),
+        },
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
