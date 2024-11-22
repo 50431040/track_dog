f3cd522ceb2a19a73d4f044a66b09050e760aaf5
Stahsf <50431040@qq.com>
2024-11-22 21:51:37 +0800

    feat: mysql、orm

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { BullModule } from '@nestjs/bull';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -33,6 +34,22 @@ import { BullModule } from '@nestjs/bull';
           port: parseInt(configService.get('REDIS_PORT'), 10),
           password: configService.get('REDIS_PASSWORD'),
         },
+      }),
+    }),
+    // 数据库
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get('DATABASE_PORT'),
+        username: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        entities: ['./**/*.entity.js'],
+        synchronize: false,
+        logging: process.env.NODE_ENV !== 'production',
       }),
     }),
   ],
