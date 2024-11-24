40567012a996a9929323a5c03ae2787a3811533b
Stahsf <50431040@qq.com>
2024-11-24 15:01:53 +0800

    feat: 别名配置、环境配置文件

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { vitePluginForArco } from '@arco-plugins/vite-react'
+import {resolve} from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), vitePluginForArco()],
+  resolve: {
+    alias: {
+      // 根路径
+      "~": resolve(__dirname, "./"),
+      // src 路径
+      "@": resolve(__dirname, "src"),
+    },
+  },
 })
